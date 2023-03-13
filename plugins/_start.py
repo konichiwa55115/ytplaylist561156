2d77148c31c3cd5e37726048cbfb240a11ec663b
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-13 19:11:27 +0200

    Update _start.py

--- plugins/_start.py
+++ plugins/_start.py
@@ -14,9 +14,10 @@ START_BUTTONS = InlineKeyboardMarkup(
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(bot, message):
-    reply_markup = START_BUTTONS
     await message.reply(
-        f"Hello {message.from_user.first_name}!\n\nThis bot can search for YouTube videos & download YouTube videos, playlists and more. "
-        f"To search videos click the below \"🔍Search Youtube\" button.\n\n"
-        f"●/playlist_aud - <i>link</i> :-To download playlist videos in audio format\n"
-        f"●/help :- For more info.", reply_markup=reply_markup)
+        f"السلام عليكم يا  {message.from_user.first_name}!\n\n هذا بوت لتحميل فيديوهات و قوائم يوتيوب سواء بصيغة فيديو أو صوت , فقط أرسل رابط الفيديو أو قائمة التشغيل   \n "
+        f"ممنوع استخدام البوت لتحميل كل ما هو حرام شرعاً , استعمله في سبيل الله \n"
+        f"●/playlist_aud - <i>رابط قائمة التشغيل </i> :لتحميل القائمة في صيغة صوتية \n " 
+        f"لبقية البوتات هنا \n https://t.me/ibnAlQyyim/1120 \n "
+        f"تم تطويره بواسطة \n sanila"
+    )
