4f8830cff79e3dcbd10bae812a1932bd568b0d1f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-13 19:13:03 +0200

    Update _start.py

--- plugins/_start.py
+++ plugins/_start.py
@@ -17,7 +17,7 @@ async def start(bot, message):
     await message.reply(
         f"السلام عليكم يا  {message.from_user.first_name}!\n\n هذا بوت لتحميل فيديوهات و قوائم يوتيوب سواء بصيغة فيديو أو صوت , فقط أرسل رابط الفيديو أو قائمة التشغيل   \n "
         f"ممنوع استخدام البوت لتحميل كل ما هو حرام شرعاً , استعمله في سبيل الله \n"
-        f"●/playlist_aud - <i>رابط قائمة التشغيل </i> :لتحميل القائمة في صيغة صوتية \n " 
+        f"●/playlist_aud - <i>playlist link </i> :لتحميل القائمة في صيغة صوتية \n " 
         f"لبقية البوتات هنا \n https://t.me/ibnAlQyyim/1120 \n "
         f"تم تطويره بواسطة \n sanila"
     )
